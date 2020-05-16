09153df69901416dd6e8454b75f80ba2f2722fc0
pputra <mynameisyoga@live.com>
2020-05-15 23:07:33 -0700

    chore: setup dependencies

---
+++ src/modules/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Home
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
