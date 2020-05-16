4cfaf2e146c457178a84ea350ae62f6d6bd98841
pputra <mynameisyoga@live.com>
2020-05-16 11:32:09 -0700

    feat: add redux dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { userReducer } from './reducers/user.reducer';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
+import { composeEnhancers } from './devTools';
+import { userReducer } from './reducers/user.reducer';
 import { AppActions } from './actions/AppActionTypes';
 
 const combinedReducers = combineReducers({
@@ -11,5 +12,6 @@ export type AppState = ReturnType<typeof combinedReducers>;
 
 export const store = createStore(
   combinedReducers,
-  applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>)
-);
+  composeEnhancers(
+    applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>)
+));
